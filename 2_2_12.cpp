8d49ab4cf7759249b68567df3cc9926b0b77504d
ikkz <miles_yang@foxmail.com>
2022-09-05 17:36:01 +0800

    feat: 2_2_12

---
+++ 2_2_12.cpp
@@ -0,0 +1,44 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include <doctest.h>
+#include <vector>
+
+using Vec = std::vector<int>;
+
+int solve(Vec vec) {
+  Vec map(vec.size(), 0);
+  std::for_each(vec.begin(), vec.end(), [&](int i) { map[i]++; });
+  for (int i = 0; i < map.size(); ++i) {
+    if (map[i] > vec.size() / 2) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+TEST_CASE("2_2_12") {
+  SUBCASE("有主元素") {
+    Vec vec = {0, 5, 5, 3, 5, 7, 5, 5};
+    CHECK_EQ(5, solve(vec));
+  }
+
+  SUBCASE("无主元素") {
+    Vec vec = {0, 5, 5, 3, 5, 1, 5, 7};
+    CHECK_EQ(-1, solve(vec));
+  }
+
+  SUBCASE("空数组") {
+    Vec vec = {};
+    CHECK_EQ(-1, solve(vec));
+  }
+
+  SUBCASE("1个元素") {
+    Vec vec = {0};
+    CHECK_EQ(0, solve(vec));
+  }
+
+  SUBCASE("2个元素") {
+    Vec vec = {1, 2};
+    CHECK_EQ(-1, solve(vec));
+  }
+}
